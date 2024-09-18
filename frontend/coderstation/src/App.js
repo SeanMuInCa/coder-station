e653991f1e0c1e107503efe44cbfea39b47b7e46
Sean <fengying1028@hotmail.com>
2024-09-17 18:51:29 -0600

    setup antd router and tailwind

--- frontend/coderstation/src/App.js
+++ frontend/coderstation/src/App.js
@@ -1,9 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<h1 className="text-3xl underline">Hello world!</h1>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
