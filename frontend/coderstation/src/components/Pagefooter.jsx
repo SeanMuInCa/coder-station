dd69a44e6f6010a7baae7768527605094fcfbbc0
Sean <fengying1028@hotmail.com>
2024-09-18 18:12:09 -0600

    footer

--- frontend/coderstation/src/components/Pagefooter.jsx
+++ frontend/coderstation/src/components/Pagefooter.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const PageFooter = () => {
   return (
-    <div>PageFooter</div>
+    <div>
+      <footer className=" text-black p-4 text-center">
+        <p>&copy; 2023 Coder Station. All rights reserved.</p>
+      </footer>
+    </div>
   )
 }
 
