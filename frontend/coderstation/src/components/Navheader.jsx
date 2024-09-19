0ece28ec2741b26261dc1fa717628fd8aef2b582
Sean <fengying1028@hotmail.com>
2024-09-18 19:20:34 -0600

    header

--- frontend/coderstation/src/components/Navheader.jsx
+++ frontend/coderstation/src/components/Navheader.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { Button } from "antd";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, message, Space, Tooltip } from "antd";
+const items = [
+	{
+		label: "Q and A",
+		key: "1",
+	},
+	{
+		label: "books",
+		key: "2",
+	},
+];
+const menuProps = {
+	items,
+};
 const NavHeader = () => {
 	return (
-		<div className="flex h-full box-border">
+		<div className="flex h-full box-border w-3/4 mx-auto justify-between ">
 			<div className="w-60">
 				<img src="/logo1.png" alt="" />
 			</div>
@@ -14,9 +28,25 @@ const NavHeader = () => {
 					<li>ddd</li>
 				</ul>
 			</div>
-			<div className="bg-white w-40"></div>
-			<div className=" w-40 flex justify-center items-center" >
-				<Button type="primary" size='large'>Login / Register</Button>
+			<div className="w-2/5 flex items-center justify-center h-full leading-1 ">
+				<div className=" w-1/4 outline-none rounded-none my-0 h-full box-border p-0 m-0 border-collapse border-none">
+					<Dropdown menu={menuProps} className="w-full h-3/4 outline-none hover:outline-none rounded-none">
+						<Button className="hover:outline-none">
+							<Space>
+								{items[0].label}
+								<DownOutlined />
+							</Space>
+						</Button>
+					</Dropdown>
+				</div>
+				<div className=" w-3/4 h-full box-border p-0 m-0 border-collapse border-none">
+          <input type="text" className="w-full h-3/4 px-3 py-1 outline-none"/>
+        </div>
+			</div>
+			<div className=" w-40 flex justify-center items-center">
+				<Button type="primary" size="large">
+					Login / Register
+				</Button>
 			</div>
 		</div>
 	);
