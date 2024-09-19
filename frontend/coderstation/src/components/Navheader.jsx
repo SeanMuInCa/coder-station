263d4c6fe34f9bb06f7662aece34906bc7aaa42e
Sean <fengying1028@hotmail.com>
2024-09-18 18:44:54 -0600

    nav header style

--- frontend/coderstation/src/components/Navheader.jsx
+++ frontend/coderstation/src/components/Navheader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "antd";
 const NavHeader = () => {
-  return (
-    <div className='text-white'>NavHeader</div>
-  )
-}
+	return (
+		<div className="flex h-full box-border">
+			<div className="w-60">
+				<img src="/logo1.png" alt="" />
+			</div>
+			<div className="text-white">
+				<ul className="flex justify-evenly w-60">
+					<li>aaa</li>
+					<li>bbb</li>
+					<li>ccc</li>
+					<li>ddd</li>
+				</ul>
+			</div>
+			<div className="bg-white w-40"></div>
+			<div className=" w-40 flex justify-center items-center" >
+				<Button type="primary" size='large'>Login / Register</Button>
+			</div>
+		</div>
+	);
+};
 
-export default NavHeader
+export default NavHeader;
