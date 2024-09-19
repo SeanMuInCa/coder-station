2c4882deb3336cad41097880707dbd5a581a1920
Sean <fengying1028@hotmail.com>
2024-09-19 12:26:06 -0600

    header

--- frontend/coderstation/src/components/Navheader.jsx
+++ frontend/coderstation/src/components/Navheader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DownOutlined } from "@ant-design/icons";
-import { Button, Dropdown, Space } from "antd";
-import { NavLink } from 'react-router-dom'
+import { Button, Select, Input, Space } from "antd";
+import { NavLink } from "react-router-dom";
 const items = [
 	{
 		label: "Q and A",
@@ -22,12 +22,14 @@ const NavHeader = () => {
 				<img src="/logo1.png" alt="" />
 			</div>
 			<div className="text-white flex justify-evenly w-1/4">
-				<NavLink to={'/'}>Questions</NavLink>
-				<NavLink to={'/books'}>Books</NavLink>
-				<NavLink to={'/interviews'}>Interviews</NavLink>
-				<a href='https://www.youtube.com' target="_blank" rel="noreferrer">Videos</a>
+				<NavLink to={"/"}>Questions</NavLink>
+				<NavLink to={"/books"}>Books</NavLink>
+				<NavLink to={"/interviews"}>Interviews</NavLink>
+				<a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+					Videos
+				</a>
 			</div>
-			<div className="w-2/5 flex items-center justify-center h-full leading-1 ">
+			{/* <div className="w-2/5 flex items-center justify-center h-full leading-1 ">
 				<div className=" w-1/4 outline-none rounded-none my-0 h-full box-border p-0 m-0 border-collapse border-none">
 					<Dropdown menu={menuProps} className="w-full h-3/4 outline-none hover:outline-none rounded-none align-middle">
 						<Button className="hover:outline-none">
@@ -41,6 +43,12 @@ const NavHeader = () => {
 				<div className=" w-3/4 h-full box-border p-0 m-0 border-collapse border-none">
           <input type="text" className="w-full h-3/4 px-3 py-1 outline-none align-middle"/>
         </div>
+			</div> */}
+			<div>
+				<Space.Compact>
+					<Select defaultValue={items[0].label} options={items} />
+					<Input placeholder="Search" className="outline-none"/>
+				</Space.Compact>
 			</div>
 			<div className=" w-40 flex justify-center items-center">
 				<Button type="primary" size="large">
