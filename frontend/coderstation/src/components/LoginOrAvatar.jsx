aa8549b82af9e7bd2a8d99759cbc854f3004a1da
Sean <fengying1028@hotmail.com>
2024-09-19 14:11:07 -0600

    reg and login

--- frontend/coderstation/src/components/LoginOrAvatar.jsx
+++ frontend/coderstation/src/components/LoginOrAvatar.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Button } from "antd";
+import { useSelector } from "react-redux";
 const LoginOrAvatar = () => {
-	const [isLogin, setIsLogin] = useState(false);
-	return (
-		<Button type="primary" size="large">
-			Login / Register
-		</Button>
-	);
+	const user = useSelector(state => state.user)
+    if(user.isLogin){
+        return (
+            <div>login</div>
+        )
+    }else{
+        return (
+            <Button type="primary" size="large">
+                Login / Register
+            </Button>
+        );
+    }
+	
 };
 
 export default LoginOrAvatar;
--- frontend/coderstation/src/redux/userSlice.js
+++ frontend/coderstation/src/redux/userSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@redux/js/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const userSlice = createSlice({
     name: 'user',
