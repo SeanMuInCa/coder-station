07d13927b86f8584add268ff09a493d876aa4352
Sean <fengying1028@hotmail.com>
2024-09-19 14:14:52 -0600

    reg and login

--- frontend/coderstation/src/components/LoginOrAvatar.jsx
+++ frontend/coderstation/src/components/LoginOrAvatar.jsx
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import { Button } from "antd";
 import { useSelector } from "react-redux";
 const LoginOrAvatar = () => {
-	const user = useSelector(state => state.user)
-    if(user.isLogin){
-        return (
-            <div>login</div>
-        )
-    }else{
-        return (
-            <Button type="primary" size="large">
-                Login / Register
-            </Button>
-        );
-    }
-	
+	const user = useSelector((state) => state.user);
+	let loginStatus = null;
+
+	if (user.isLogin) {
+		loginStatus = <div>login</div>
+	} else {
+		loginStatus = (
+			<Button type="primary" size="large">
+				Login / Register
+			</Button>
+		);
+	}
+	return <div>{loginStatus}</div>;
 };
 
 export default LoginOrAvatar;
