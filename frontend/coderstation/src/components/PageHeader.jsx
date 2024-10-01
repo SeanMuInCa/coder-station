182f80407d67cbeeb1a2a9f684347e80052d75c7
Sean <fengying1028@hotmail.com>
2024-10-01 14:43:24 -0600

    page headers

--- frontend/coderstation/src/components/PageHeader.jsx
+++ frontend/coderstation/src/components/PageHeader.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Tag, Flex } from "antd";
+const PageHeader = (props) => {
+	return (
+		<div className="flex py-10">
+			<h1 className="text-4xl font-bold  flex-0.5 mx-10">{props.title}</h1>
+			<div className="flex-1 flex items-center">
+				<Flex gap="4px 0" wrap className="flex items-end justify-start my-auto cursor-pointer">
+					<Tag color="#f50">#f50</Tag>
+					<Tag color="#2db7f5">#2db7f5</Tag>
+					<Tag color="#87d068">#87d068</Tag>
+					<Tag color="#108ee9">#108ee9</Tag>
+				</Flex>
+			</div>
+		</div>
+	);
+};
 
-const PageHeader = () => {
-  return (
-    <div>PageHeader</div>
-  )
-}
-
-export default PageHeader
+export default PageHeader;
--- frontend/coderstation/src/pages/Issues.jsx
+++ frontend/coderstation/src/pages/Issues.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import PageHeader from '../components/PageHeader'
 const Issues = () => {
   return (
-    <div>Issues</div>
+    <div className='max-w-7xl mx-auto bg-gray-200'>
+      <PageHeader title="Issues" />
+    </div>
+    
   )
 }
 
