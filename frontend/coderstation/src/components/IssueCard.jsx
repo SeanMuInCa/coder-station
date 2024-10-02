785f35b6e20aa3f1eb363845132436a3b12a907d
Sean <fengying1028@hotmail.com>
2024-10-02 14:57:56 -0600

    style

--- frontend/coderstation/src/components/IssueCard.jsx
+++ frontend/coderstation/src/components/IssueCard.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import { Tag } from 'antd'
 const IssueCard = (props) => {
-    
+    const info = props.info
   return (
-    <div className='mx-16 py-5 flex'>
-        <div className='bg-yellow-300 flex-0.5 flex p-10'>
-            <div className='mr-20'>
-                <p className='text-center'>{props.info.commentNumber}</p>
+    <div className='mx-16 py-5 flex border-b-2'>
+        <div className='flex-0.5 flex p-5'>
+            <div className='mr-16 flex flex-col justify-between'>
+                <p className='text-center'>{info.commentNumber}</p>
                 <p>Comments</p>
             </div>
-            <div>
-                <p className='text-center'>{props.info.scanNumber}</p>
+            <div className='mr-16 flex flex-col justify-between'>
+                <p className='text-center'>{info.scanNumber}</p>
                 <p>Views</p>
             </div>
         </div>
-        <div className='bg-blue-400 flex-1 pt-10 px-5'>
-            <p>{props.info.issueTitle}</p>
+        <div className='flex-1 pt-5 px-5'>
+            <p>{info.issueTitle}</p>
             <div className='pb-2 mt-8 flex justify-between'>
                 <div>
                     <Tag color="green">123</Tag>
                 </div>
                 <div>
                     <Tag color="blue">456</Tag>
-                    <span>{props.info.issueDate}</span>
+                    <span>{info.issueDate}</span>
                 </div>
             </div>
         </div>
