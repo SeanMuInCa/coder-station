13dc2313c358022bea30f9b3e4988a79c09cf271
Sean <fengying1028@hotmail.com>
2024-10-02 14:20:30 -0600

    render list

--- frontend/coderstation/src/pages/Issues.jsx
+++ frontend/coderstation/src/pages/Issues.jsx
@@ -4,31 +4,37 @@ import { getIssueListApi } from '../api/issue'
 const Issues = () => {
   const [issueList, setIssueList] = useState([]);
   const [pageInfo, setPageInfo] = useState({
-    current:1,
-    pageSize:10,
-    total: 0
+    current:1,//current page
+    pageSize:10,// how many in one page
+    total: 0//total amount
   });
   useEffect(() => {
+    const getIssueList = async () => {
+      const res = await getIssueListApi({
+        current: pageInfo.current,
+        pageSize: pageInfo.pageSize,
+        issueStatus: true
+      })
+      console.log(res,'res')
+      setIssueList(res.data.data);
+      setPageInfo({
+        current: res.data.currentPage,
+        pageSize: res.data.eachPage,
+        total: res.data.count
+      })
+      // if(res.code === 0){
+      //   const arr = res.data.data.filter(item=> item.issueStatus === true)
+      //   setIssueList(arr)
+      // }
+    };
     getIssueList();
   }, [pageInfo.current, pageInfo.pageSize]);
-  const getIssueList = async () => {
-    const res = await getIssueListApi({
-      current: pageInfo.current,
-      pageSize: pageInfo.pageSize,
-      issueStatus: true
-    })
-    console.log(res)
-    setIssueList(res.data.data);
-    setPageInfo({
-      current: res.data.current,
-      pageSize: res.data.pageSize,
-      total: res.data.total
-    })
-    // if(res.code === 0){
-    //   const arr = res.data.data.filter(item=> item.issueStatus === true)
-    //   setIssueList(arr)
-    // }
-  };
+  
+  let list = issueList.map(item => (
+    <div key={item._id}>
+      {item.title}
+    </div>
+  ))
   return (
     <div className='max-w-7xl mx-auto bg-slate-50'>
       <PageHeader title="Issue List" />
