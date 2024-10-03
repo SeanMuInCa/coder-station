0f8cb1a2c923d0527cb303ce2476c32ec43cfae0
Sean <fengying1028@hotmail.com>
2024-10-03 14:57:11 -0600

    date

--- frontend/coderstation/src/components/IssueCard.jsx
+++ frontend/coderstation/src/components/IssueCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Tag } from 'antd'
 import { getUserInfo } from '../api/user'
 import { useSelector } from 'react-redux'
+import { format } from 'date-fns'
 const IssueCard = (props) => {
     const [nickname, setNickname] = useState('');
     const [type, setType] = useState('');
@@ -28,8 +29,8 @@ const IssueCard = (props) => {
         })
     },[info.typeId, typeInfo.type])
   return (
-    <div className='mx-16 py-5 flex border-b-2'>
-        <div className='flex-0.5 flex p-5 text-gray-400'>
+    <div className='mx-16 pt-5 pb-2 flex border-b-2'>
+        <div className='flex-0.5 flex py-6 px-5 text-gray-400'>
             <div className='mr-16 flex flex-col justify-between'>
                 <p className='text-center'>{info.commentNumber}</p>
                 <p>Comments</p>
@@ -39,15 +40,15 @@ const IssueCard = (props) => {
                 <p>Views</p>
             </div>
         </div>
-        <div className='flex-1 pt-5 px-5'>
-            <p>{info.issueTitle}</p>
+        <div className='flex-1 pt-3 px-5'>
+            <p className='text-black'>{info.issueTitle}</p>
             <div className='pb-2 mt-8 flex justify-between'>
                 <div>
                     <Tag color="green">{type}</Tag>
                 </div>
                 <div>
                     <Tag color="blue">{nickname}</Tag>
-                    <span>{info.issueDate}</span>
+                    <span>{format(new Date(parseFloat(info.issueDate)), 'yyyy-MM-dd')}</span>
                 </div>
             </div>
         </div>
