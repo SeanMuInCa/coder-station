88dac7126b0cee59e3bbc9c7a313660503eda20c
Sean <fengying1028@hotmail.com>
2024-10-04 15:31:46 -0600

    still bug

--- frontend/coderstation/src/components/Category.jsx
+++ frontend/coderstation/src/components/Category.jsx
@@ -1,10 +1,9 @@
 import { Tag, Flex } from "antd";
 import { useSelector } from "react-redux";
-import React from "react";
+
 
 const Category = (props) => {
     const  {type}  = useSelector((state) => state.type);
-    console.log(type);
 	return (
 		<Flex
 			gap="4px 0"
