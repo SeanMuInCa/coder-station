01270c3f95f029101c3d3c48e30508618767e39b
Sean <fengying1028@hotmail.com>
2024-10-10 14:04:41 -0600

    recommendation

--- frontend/coderstation/src/components/RecommendItem.jsx
+++ frontend/coderstation/src/components/RecommendItem.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 function RecommendItem(props) {
     return (
-        <a  href={props.recommendInfo.href} target="_blank" rel="noreferrer">
-            <div >{props.recommendInfo.num}</div>
+        <a className='flex border-b-2 mb-4' href={props.recommendInfo.href} target="_blank" rel="noreferrer">
+            <div className='w-6 bg-blue-500 text-white mb-5 mr-2 rounded-md'>{props.recommendInfo.num}</div>
             <div >{props.recommendInfo.title}</div>
         </a>
     );
