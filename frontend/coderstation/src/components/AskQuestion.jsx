7df3f3805cd5aacc731cb2465f0fc0f0112b6676
Sean <fengying1028@hotmail.com>
2024-10-11 14:48:05 -0600

    add question

--- frontend/coderstation/src/components/AskQuestion.jsx
+++ frontend/coderstation/src/components/AskQuestion.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Form, Input } from "antd";
+import TextArea from "antd/es/input/TextArea";
 const AskQuestion = () => {
-  return (
-    <Card className='max-w-7xl mx-auto bg-slate-50 pb-10'>AskQuestion</Card>
-  )
-}
+    const [question, setQuestion] = useState({
+        issueTitle: "",
+        issueContent: "",
+        userId:'',
+        typeId:''
+    })
+	return (
+		<Card className="max-w-7xl mx-auto bg-slate-50 pb-10">
+			<Form>
+				<Form.Item
+					name={"title"}
+					label="Question Title"
+					rules={[
+						{
+							required: true,
+							message: "Please input your title!",
+						},
+					]}
+				>
+					<Input placeholder="Ask a public question" value={question.issueTitle}/>
+				</Form.Item>
+				<Form.Item
+					name={"content"}
+					label="Question Content"
+					rules={[
+						{
+							required: true,
+							message: "Please input your content!",
+						},
+					]}
+				>
+					<TextArea
+						showCount
+                        value={question.issueContent}
+						maxLength={100}
+						//   onChange={onChange}
+						style={{
+							height: 120,
+							resize: "none",
+						}}
+					/>
+				</Form.Item>
+			</Form>
+		</Card>
+	);
+};
 
-export default AskQuestion
+export default AskQuestion;
