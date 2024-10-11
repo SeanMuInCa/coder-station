58cfe34c0d68d2c0f48005fa6c7e55edbaa5d348
Sean <fengying1028@hotmail.com>
2024-10-11 14:57:23 -0600

    ask question

--- frontend/coderstation/src/components/AskQuestion.jsx
+++ frontend/coderstation/src/components/AskQuestion.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import { Button, Card, Form, Input } from "antd";
+import { Button, Card, Form, Input, Select } from "antd";
 import TextArea from "antd/es/input/TextArea";
 const AskQuestion = () => {
-    const [question, setQuestion] = useState({
-        issueTitle: "",
-        issueContent: "",
-        userId:'',
-        typeId:''
-    })
-    const handleClick = ()=>{
-        console.log(question)
-    }
+	const [question, setQuestion] = useState({
+		issueTitle: "",
+		issueContent: "",
+		userId: "",
+		typeId: "",
+	});
+	const handleClick = () => {
+		console.log(question);
+	};
 	return (
 		<Card className="max-w-7xl mx-auto bg-slate-50 pb-10">
 			<Form onFinish={handleClick}>
@@ -24,7 +24,25 @@ const AskQuestion = () => {
 						},
 					]}
 				>
-					<Input placeholder="Ask a public question" value={question.issueTitle} onChange={(e)=>setQuestion({...question,issueTitle:e.target.value})}/>
+					<Input
+						placeholder="Ask a public question"
+						value={question.issueTitle}
+						onChange={(e) => setQuestion({ ...question, issueTitle: e.target.value })}
+					/>
+				</Form.Item>
+				<Form.Item
+					name={"type"}
+					label="Question Type"
+					rules={[
+						{
+							required: true,
+						},
+					]}
+                    className="w-1/5"
+				>
+					<Select>
+						<Select.Option value="rmb">RMB</Select.Option>
+					</Select>
 				</Form.Item>
 				<Form.Item
 					name={"content"}
@@ -38,20 +56,22 @@ const AskQuestion = () => {
 				>
 					<TextArea
 						showCount
-                        value={question.issueContent}
+						value={question.issueContent}
 						maxLength={100}
-						onChange={(e)=>setQuestion({...question,issueContent:e.target.value})}
+						onChange={(e) =>
+							setQuestion({ ...question, issueContent: e.target.value })
+						}
 						style={{
 							height: 120,
 							resize: "none",
 						}}
 					/>
 				</Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
+				<Form.Item>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
 			</Form>
 		</Card>
 	);
