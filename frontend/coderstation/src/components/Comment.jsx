6176b07cff2bfc7e133111c2c057e217771c8787
Sean <fengying1028@hotmail.com>
2024-10-16 17:21:34 -0600

    comment

--- frontend/coderstation/src/components/Comment.jsx
+++ frontend/coderstation/src/components/Comment.jsx
@@ -1,32 +1,41 @@
-import React, { useEffect,useRef } from 'react'
-import { useSelector } from 'react-redux'
-import '@toast-ui/editor/dist/toastui-editor.css'
-import { Editor } from '@toast-ui/react-editor'
-import { Button } from 'antd'
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "antd";
 const Comment = (props) => {
-    const editorRef = useRef();
-    const user = useSelector(state => state.user)
-    useEffect(()=>{
-        
-    },[])
-  return (
-    <div className=' ml-10 rounded-md p-5 mr-5 text-center'>
-        {/* Comment detail */}
-        <div>
-        {props.commentList.length === 0 ? <div>No comments</div> : <div>1</div>}
-        </div>
-        {/* textarea */}
-        <div>
-        {user.isLogin ? <Editor
+	const editorRef = useRef();
+	const user = useSelector((state) => state.user);
+    console.log(props);
+    
+	useEffect(() => {}, []);
+	return (
+		<div className=" ml-5 rounded-md p-5  text-center">
+			{/* Comment detail */}
+			<div className="px-5 my-5">
+				{props.commentList.length === 0 ? <div>No comments</div> : <div>1</div>}
+			</div>
+			{/* textarea */}
+			<div>
+				{user.isLogin ? (
+					<>
+                    <Editor
 						previewStyle="vertical"
 						height="300px"
 						initialEditType="wysiwyg"
 						useCommandShortcut={true}
 						ref={editorRef}
-					/> : <Button type='primary'>Please login to comment</Button>}
-        </div>
-    </div>
-  )
-}
+					/>
+                    <Button type="primary" className="mt-5">Comment Submit</Button>
+                    </>
+                    
+				) : (
+					<Button type="primary">Please login to comment</Button>
+				)}
+			</div>
+            
+		</div>
+	);
+};
 
-export default Comment
+export default Comment;
