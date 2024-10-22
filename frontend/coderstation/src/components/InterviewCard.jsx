0a66309ca93ad4655aa7fa1dd81b53c901f78376
Sean <fengying1028@hotmail.com>
2024-10-22 10:11:30 -0600

    interview done

--- frontend/coderstation/src/components/InterviewCard.jsx
+++ frontend/coderstation/src/components/InterviewCard.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Card, Collapse } from "antd";
 import { useNavigate } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 const InterviewCard = (props) => {
+	const user = useSelector((state) => state.user);
     const navigate = useNavigate();
     const handleClick = ()=>{
         navigate(props.interview._id)
     }
+	let label = user.isLogin ? 'Check the answer' : 'Login to check the answer'
 	return (
 		<Card className="my-5" 
         title={props.interview.interviewTitle}
-        extra={<span className="text-blue-500 font-bold cursor-pointer" onClick={handleClick}>Open in New Tab</span>}
+        extra={user.isLogin?<span className="text-blue-500 font-bold cursor-pointer" onClick={handleClick}>Open in New Tab</span>:<span>Open in New Tab</span>}
         >
 			<Collapse
 				size="large"
+				collapsible={user.isLogin ? '' : "disabled"}
 				items={[
 					{
 						key: "1",
-						label: "Check the answer",
+						label,
 						children: (
 							<div
                                 className="max-h-80 overflow-y-auto text-balance ..."
