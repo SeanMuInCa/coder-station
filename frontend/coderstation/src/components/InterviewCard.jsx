879c5866916ff1421e15fc75887f9a51318bf879
Sean <fengying1028@hotmail.com>
2024-10-22 09:11:40 -0600

    interview card

--- frontend/coderstation/src/components/InterviewCard.jsx
+++ frontend/coderstation/src/components/InterviewCard.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Card } from 'antd';
+import React from "react";
+import { Card, Collapse } from "antd";
 
 const InterviewCard = (props) => {
-    console.log(props);
-    
-  return (
-    <Card
-    className='my-5'
-        title={props.interview.interviewTitle}
-    >
-        {/* <div
-            dangerouslySetInnerHTML={{ __html: props.interview.interviewContent }}
-          ></div> */}
-    </Card>
-  )
-}
+	return (
+		<Card className="my-5" title={props.interview.interviewTitle}>
+			<Collapse
+				size="large"
+				items={[
+					{
+						key: "1",
+						label: "Check the answer",
+						children: (
+							<div
+								dangerouslySetInnerHTML={{ __html: props.interview.interviewContent }}
+							></div>
+						),
+					},
+				]}
+			/>
+		</Card>
+	);
+};
 
-export default InterviewCard
+export default InterviewCard;
