3a0ba9ffec2aa0c8b76e7dfa961013619f68fb07
Sean <fengying1028@hotmail.com>
2024-10-30 11:38:58 -0600

    book search

--- frontend/coderstation/src/pages/Books.jsx
+++ frontend/coderstation/src/pages/Books.jsx
@@ -3,6 +3,7 @@ import { getAllBookApi } from "../api/book";
 import PageHeader from "../components/PageHeader";
 import BookCard from "../components/BookCard";
 import { Pagination } from "antd";
+import { useSelector } from 'react-redux'
 const Books = () => {
 	const [pageInfo, setPageInfo] = useState({
 		current: 1,
@@ -10,6 +11,7 @@ const Books = () => {
 		total: 0,
 	});
 	const [bookList, setBookList] = useState([]);
+	const search = useSelector(state => state.search);
 	useEffect(() => {
 		const fetchData = async () => {
 			const res = await getAllBookApi({
@@ -27,17 +29,26 @@ const Books = () => {
 		};
 		fetchData();
 	}, [pageInfo.current, pageInfo.pageSize]);
-	let list = bookList.data?.map((item) => (
-		<BookCard key={item._id} book={item} />
-	));
+	// let list = bookList.data?.map((item) => (
+	// 	<BookCard key={item._id} book={item} />
+	// ));
+	useEffect(() => {
+		const filteredList = search.searchMode
+		  ? bookList
+			  .filter((item) => item.issueTitle.includes(search.SearchInfo?.keyWord)) // 根据关键词过滤
+			  .map((item) => <BookCard info={item} key={item._id} />)
+		  : bookList.map((item) => <BookCard info={item} key={item._id} />);
+		
+		  setBookList(filteredList);  // 设置新的列表
+	  }, [search.searchMode, search.SearchInfo?.keyWord, issueList]); // 监听search.searchMode和关键词变化
 	return (
 		<>
 			<div className="max-w-7xl mx-auto bg-slate-50">
 				<PageHeader hideCategory={true} title="Book List" />
 			</div>
 			<div className="flex max-w-7xl mx-auto bg-slate-50 pb-10 justify-evenly flex-wrap">
-				{list?.length ? (
-					list
+				{bookList?.length ? (
+					bookList
 				) : (
 					<div className="text-center text-2xl text-gray-400 mx-auto">
 						No book found
