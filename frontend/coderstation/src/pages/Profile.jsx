a0896e7e2c878cf0b063304d1bbe62371f8d1d85
Sean <fengying1028@hotmail.com>
2024-10-31 14:39:23 -0600

    profile basic

--- frontend/coderstation/src/pages/Profile.jsx
+++ frontend/coderstation/src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import { format } from 'date-fns';
 const Profile = () => {
   const {id} = useParams();
   const [user, setUser] = useState({})
-
+  
   useEffect(()=>{
     const fetchData = async()=>{
       const res = await getUserInfo(id);
@@ -25,8 +25,9 @@ const Profile = () => {
         <div className='p-10 text-xl flex flex-col gap-5'>
           <div><span>User Nickname: </span>{user.nickname}</div>
           <div><span>User Points: </span>{user.points}</div>
-          <div><span>Last Login Date: </span>{format(new Date(parseFloat(user.lastLoginDate)), 'yyyy-MM-dd')}</div>
-          <div><span>Register Date: </span>{format(new Date(parseFloat(user.registerDate)), 'yyyy-MM-dd')}</div>
+          <div><span>User Status: </span>{user.enabled ?'Normal':'Frozen'}</div>
+          <div><span>Last Login Date: </span>{user.lastLoginDate ? format(new Date(parseFloat(user.lastLoginDate)), 'yyyy-MM-dd') : 'N/A'}</div>
+          <div><span>Register Date: </span>{user.registerDate ? format(new Date(parseFloat(user.registerDate)), 'yyyy-MM-dd') : 'N/A'}</div>
         </div>
       </div>
     </div>
