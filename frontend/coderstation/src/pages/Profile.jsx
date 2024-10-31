e8629765319e28c1a906233b77be3374c8cbabd6
Sean <fengying1028@hotmail.com>
2024-10-31 14:14:03 -0600

    profile

--- frontend/coderstation/src/pages/Profile.jsx
+++ frontend/coderstation/src/pages/Profile.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { getUserInfo } from '../api/user'; 
 const Profile = () => {
+  const {id} = useParams();
+  const user = useSelector(state=>state.user);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const res = await getUserInfo(id);
+      console.log(res)
+    }
+    fetchData();
+  },[id])
+  console.log(id)
   return (
     <div className="max-w-7xl mx-auto bg-white pb-10">
       Profile
