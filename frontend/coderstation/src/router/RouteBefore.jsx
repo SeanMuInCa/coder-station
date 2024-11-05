de5bb482090a0d73364422389327e3bb48fb45ba
Sean <fengying1028@hotmail.com>
2024-11-05 16:59:59 -0600

    remove useless codes

--- frontend/coderstation/src/router/RouteBefore.jsx
+++ frontend/coderstation/src/router/RouteBefore.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux"; // 假设你使用 Redux 存储用户状态
 
 const RouteBefore = ({ children, requireLogin }) => {
 	const navigate = useNavigate();
@@ -13,7 +12,6 @@ const RouteBefore = ({ children, requireLogin }) => {
 		}
 	}, [requireLogin, isLogin, navigate, location.pathname]);
 
-	// 如果无需登录或用户已登录，则渲染子组件，否则不渲染
 	return children;
 };
 
