d2f8c6790d4aa9113c7d67e548e29af7a77f82cb
Sean <fengying1028@hotmail.com>
2024-11-05 11:14:04 -0600

    simple version of route guardian

--- frontend/coderstation/src/router/RouteBefore.jsx
+++ frontend/coderstation/src/router/RouteBefore.jsx
@@ -1,1 +1,23 @@
 import config from "./routeBeforeConfig";
+
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux'; // 假设你使用 Redux 存储用户状态
+
+const RouteBefore = ({ children, requireLogin  }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const user = useSelector((state) => state.user);
+  
+    useEffect(() => {
+      if (requireLogin && !user.isLoggedIn) {
+        // 如果需要登录但用户未登录，则重定向到登录页面
+        navigate('/issues', { state: { from: location.pathname } });
+      }
+    }, [requireLogin, user.isLoggedIn, navigate, location.pathname]);
+  
+    // 如果无需登录或用户已登录，则渲染子组件，否则不渲染
+    return !requireLogin || user.isLoggedIn ? children : null;
+};
+
+export default RouteBefore;
--- frontend/coderstation/src/router/index.jsx
+++ frontend/coderstation/src/router/index.jsx
@@ -6,18 +6,23 @@ import IssueDetail from "../pages/IssueDetail";
 import AskQuestion from "../pages/AskQuestion";
 import BookDetail from '../pages/BookDetail';
 import InterviewDetail from "../pages/InterviewDetail";
-import Profile from '../pages/Profile'
-export default function RouteConfig(props){
+import Profile from '../pages/Profile';
+import RouteBefore from "./RouteBefore";
+import config from './routeBeforeConfig'; // 引入配置文件
+export default function RouteConfig(){
+    const getObj = (path) => {
+        return config[config.findIndex(item => item.path === path)]
+    }
     return (
         <Routes>
-            <Route path="/issues" element={<Issues keyWord={props.keyWord} />} />
+            <Route path={getObj('/issues').path} element={<RouteBefore requireLogin={getObj('/issues').requireLogin}><Issues/></RouteBefore>} />
             <Route path="/issues/:id" element={<IssueDetail />}/>
-            <Route path="/issues/add" element={<AskQuestion />} />
-            <Route path="/books" element={<Books />} keyWord={props.keyWord}/>
+            <Route path={getObj("/issues/add").path} element={<RouteBefore requireLogin={getObj("/issues/add").requireLogin}><AskQuestion/></RouteBefore>} />
+            <Route path="/books" element={<Books />}/>
             <Route path="/books/:id" element={<BookDetail />}/>
             <Route path="/interviews" element={<Interviews />} />
             <Route path="/interviews/:id" element={<InterviewDetail />} />
-            <Route path="/profile/:id" element={<Profile />} />
+            <Route path={getObj("/profile/:id").path} element={<RouteBefore requireLogin={getObj("/profile/:id").requireLogin}><Profile /></RouteBefore>} />
             <Route path="*" element={<Navigate replace to="/issues" />} />
         </Routes>
     );
