0c8a807f7895e926fe0c0101fbf0f0204f2acc30
Sean <fengying1028@hotmail.com>
2024-11-07 19:22:32 -0600

    code style

--- frontend/coderstation/src/components/CommentCard.jsx
+++ frontend/coderstation/src/components/CommentCard.jsx
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { Card, Avatar } from 'antd'
-import { getUserInfo } from '../api/user'
-import { format } from 'date-fns';
+import React, { useEffect, useState } from "react";
+import { Card, Avatar } from "antd";
+import { getUserInfo } from "../api/user";
+import { format } from "date-fns";
 
 const CommentCard = (props) => {
-    const [publisher, setPublisher] = useState({})
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await getUserInfo(props.commentInfo.userId)
-            if(res.code === 0) setPublisher(res.data)
-        }
-        if(props.commentInfo){
-            fetchData()
-        }
-    }, [])
-  return (
-    <Card>
-        <Avatar src={publisher.avatar} className='mr-2'/>
-        <span className='text-gray-400'>{format(new Date(parseFloat(props.commentInfo.commentDate)), 'yyyy-MM-dd hh:mm:ss')}</span>
-        <p className='ml-10 text-gray-400 mb-8'>{publisher.nickname}</p>
-       
-        <p className='text-left px-5' dangerouslySetInnerHTML={{ __html: props.commentInfo.commentContent }} />
-    </Card>
-  )
-}
+	const [publisher, setPublisher] = useState({});
 
-export default CommentCard
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await getUserInfo(props.commentInfo.userId);
+			if (res.code === 0) setPublisher(res.data);
+		};
+		if (props.commentInfo) {
+			fetchData();
+		}
+	}, []);
+	return (
+		<Card>
+			<Avatar src={publisher.avatar} className="mr-2" />
+			<span className="text-gray-400">
+				{format(
+					new Date(parseFloat(props.commentInfo.commentDate)),
+					"yyyy-MM-dd hh:mm:ss"
+				)}
+			</span>
+			<p className="ml-10 text-gray-400 mb-8">{publisher.nickname}</p>
+
+			<p
+				className="text-left px-5"
+				dangerouslySetInnerHTML={{ __html: props.commentInfo.commentContent }}
+			/>
+		</Card>
+	);
+};
+
+export default CommentCard;
